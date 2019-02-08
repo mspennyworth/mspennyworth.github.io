eb6e61b86f83d9117e77ee34dda521f74cdc2e43
mspennyworth <sarah13mail@gmail.com>
2019-02-07 21:03:33 -0500

    Added egengineering files

---
+++ egengineering/resupply/San Francisco Examiner_files/browse.js
@@ -0,0 +1,8 @@
+<!--Hide script from old browsers
+
+function openWin(URL) {
+	aWindow = window.open(URL, "thewindow", "toolbar=no,width=500,height=550,scrollbars=yes,status=no,resize=yes,menubar=no")
+	aWindow.focus()
+}
+
+// End hiding script from old browsers -->
---
+++ egengineering/resupply/San Francisco Examiner_files/photowindow.js
@@ -0,0 +1,8 @@
+<!--Hide script from old browsers
+
+function newWindow(photovert) {
+	cartoonWindow = window.open(photovert, "photoWin", "width=500,height=550,scrollbars=yes")
+	cartoonWindow.focus()
+}
+
+// End hiding script from old browsers -->
